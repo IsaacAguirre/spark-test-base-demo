1d327b8d3f615fbcb06a30dd1532f269b22672c3
Baris Ergun <bergun@Bariss-MBP.lan>
2019-02-08 13:56:09 +0000

    #1 adding a sample case class and a custom dataset generator

--- src/test/scala/com/pairwiseltd/spark/test/demo/SampleDatasetGeneratorTest.scala
+++ src/test/scala/com/pairwiseltd/spark/test/demo/SampleDatasetGeneratorTest.scala
@@ -1,7 +1,8 @@
 package com.pairwiseltd.spark.test.demo
 
 import com.holdenkarau.spark.testing.{DatasetGenerator, DatasetSuiteBase}
-import org.scalacheck.Arbitrary
+import org.apache.spark.sql.Dataset
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
 import org.scalacheck.Prop.forAll
@@ -11,12 +12,25 @@ class SampleDatasetGeneratorTest extends FunSuite with DatasetSuiteBase with Che
   test("test generating Datasets[String]") {
     import sqlContext.implicits._
 
-    val property =
-      forAll(DatasetGenerator.genDataset[String](sqlContext)(Arbitrary.arbitrary[String])) {
-        dataset => dataset.map(_.length).count() == dataset.count()
+    val personGen: Gen[Dataset[Person]] =
+      DatasetGenerator.genDataset[Person](sqlContext) {
+        val generator: Gen[Person] = for {
+          name <- Arbitrary.arbitrary[String]
+          speed <- Arbitrary.arbitrary[Byte]
+        } yield (Person(name, speed))
+
+        generator
       }
 
-    check(property)
+    val property =
+      forAll(personGen) {
+        dataset => dataset.map(_.age).count() == dataset.count()
+      }
+
+    property.check
   }
 
 }
+
+
+case class Person(name: String, age: Byte /* a person not a vampire*/)
